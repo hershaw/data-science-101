7370c068636566a257fdf9b11a318e0d079c5002
Samuel Hopkins <sam@crowdprocess.com>
2017-01-28 15:56:32 +0000

    first pass at re-organizing

--- Vagrantfile
+++ Vagrantfile
@@ -9,7 +9,7 @@ Vagrant.configure(2) do |config|
     v.cpus = 4
   end
 
-  config.vm.synced_folder "classes", "/home/vagrant/classes", type: "nfs"
+  config.vm.synced_folder "course", "/home/vagrant/course", type: "nfs"
   config.vm.network "private_network", ip: "192.168.50.13"
   config.vm.network "forwarded_port", guest: 8888, host: 8888
 
---
+++ course/codefiles/datagen.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+
+def random_xy(num_points=100):
+    return pd.DataFrame({
+        'x': np.random.rand(num_points),
+        'y': np.random.rand(num_points)
+    })
+
+
+def x_plus_noise(num_points=100, slope=1, randomness=0.1):
+    if not (0 <= randomness <= 1):
+        raise ValueError('randomness must be between 0 and 1 (inclusive).')
+    x = np.random.rand(num_points)
+    return pd.DataFrame({
+        'x': x,
+        'y': slope * x + np.random.normal(scale=randomness, size=num_points)
+    })
+
+
+def data_3d(num_points=100, randomness=0.1):
+    data = pd.DataFrame({
+        'x': np.random.normal(scale=10, size=num_points),
+        'y': np.random.normal(scale=1, size=num_points),
+        'z': np.random.normal(scale=randomness, size=num_points)
+    })
+    return pd.DataFrame(
+        (np.dot(_rot_mat_x(), np.dot(_rot_mat_z(), data.values.transpose()))).transpose(),
+        columns=['x', 'y', 'z']
+    )
+
+
+def _rot_mat_x(theta=np.radians(30)):
+    return np.array([
+        [1, 0, 0],
+        [0, np.cos(theta), -np.sin(theta)],
+        [0, np.sin(theta), np.cos(theta)]
+    ])
+
+
+def _rot_mat_z(theta=np.radians(60)):
+    return np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
+        [0, 0, 1]
+    ])
+
