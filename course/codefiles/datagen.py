fca00b4ba6193ba546013d7091ec0134ca4cc31c
Ines <ialmeida@lqd.io>
2017-01-28 17:39:57 +0000

    Switch np.random.rand to np.random.normal.

--- course/codefiles/datagen.py
+++ course/codefiles/datagen.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 def random_xy(num_points=100):
     return pd.DataFrame({
-        'x': np.random.rand(num_points),
-        'y': np.random.rand(num_points)
+        'x': np.random.normal(num_points),
+        'y': np.random.normal(num_points)
     })
 
 
